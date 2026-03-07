4311cc349d7b7225f518c9370bee0d62fa0e2914
rredel-hash <rredel@gmail.com>
2026-03-07 13:59:18 -0300

    Update scrape.py

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -52,4 +52,7 @@ df.drop_duplicates(
 cutoff = pd.Timestamp.now() - pd.Timedelta(days=7)
 df = df[df.timestamp >= cutoff]
 
+import os
+os.makedirs("data", exist_ok=True)
+
 df.to_parquet(RAW_FILE, index=False)
